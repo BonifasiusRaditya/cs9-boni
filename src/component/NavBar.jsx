b2b35927974f5751ffd22abf6b1d015ed22208d0
BonifasiusRaditya <radityahendrianto@gmail.com>
2025-04-30 18:41:02 +0700

    second

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -3,16 +3,16 @@ import React from "react";
 
 function NavBar({ darkMode, toggleMode }) {
     return(
-        <nav className={`flex justify-between items-center px-10 py-4 bg-teal-400`}>
+        <nav className={`flex justify-between items-center px-10 py-4 bg-cyan-400`}>
           <div className='flex flex-col items-center'>
-            <div className="text-2xl text-white font-bold">KanStore</div>
-            <div className="text-sm text-gray-400 text-center">Kantas Store</div>
+            <div className="text-2xl text-black font-bold">KanStore</div>
+            <div className="text-sm text-gray-800 text-center">Kantas Store</div>
           </div>
-          <div className="flex space-x-6 font-white">
-            <a href="#" className="hover:text-blue">Profile</a>
-            <a href="#" className="hover:text-blue">Produk</a>
-            <a href="#" className="hover:text-blue">Contact</a>
-            <a href="#" className="hover:text-blue">About Us</a>
+          <div className="flex space-x-6 text-black">
+            <a href="#" className="text-inherit hover:text-white">Profile</a>
+            <a href="#" className="text-inherit hover:text-white">Produk</a>
+            <a href="#" className="text-inherit hover:text-white">Contact</a>
+            <a href="#" className="text-inherit hover:text-white">About Us</a>
           </div>
         </nav>
     )
